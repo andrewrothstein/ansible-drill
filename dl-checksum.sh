fa2421d2e84b43cbe369267cf1568246640fb3df
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-02-01 16:27:16 -0500

    up to v1.17.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
-VER=1.16.0
-URL=https://www.apache.org/dist/drill/drill-${VER}/apache-drill-${VER}.tar.gz.sha512
-printf "  # %s\n" $URL
-printf "  '%s': sha512:%s\n" $VER `curl -sSL $URL`
+MIRROR=https://www.apache.org/dist/drill
+
+dl_ver() {
+    local ver=$1
+    local url=$MIRROR/drill-${ver}/apache-drill-${ver}.tar.gz.sha512
+    printf "  # %s\n" $url
+    printf "  '%s': sha512:%s\n" $ver $(curl -sSL $url)
+}
+
+dl_ver ${1:-1.17.0}
