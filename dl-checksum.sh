7c8821ff149ac71ba2fae730ffd01c257273ddc6
Andrew Rothstein <andrew.rothstein@gmail.com>
2021-09-05 17:10:56 -0400

    apache-drill:1.19.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -3,9 +3,9 @@ MIRROR=https://www.apache.org/dist/drill
 
 dl_ver() {
     local ver=$1
-    local url=$MIRROR/drill-${ver}/apache-drill-${ver}.tar.gz.sha512
+    local url="${MIRROR}/drill-${ver}/apache-drill-${ver}.tar.gz.sha512"
     printf "  # %s\n" $url
     printf "  '%s': sha512:%s\n" $ver $(curl -sSL $url)
 }
 
-dl_ver ${1:-1.17.0}
+dl_ver ${1:-1.19.0}
