cef4d1a9cabeb23e65d6015cf9bffa7233117075
Andrew Rothstein <andrew.rothstein@gmail.com>
2024-04-01 21:46:57 +0000

    apache-drill:1.21.1

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env sh
-MIRROR=https://www.apache.org/dist/drill
+set -e
+MIRROR=https://dlcdn.apache.org/drill
+DIR=~/Downloads
+
+# https://dlcdn.apache.org/drill/1.21.1/apache-drill-1.21.1.tar.gz
 
 dl_ver() {
     local ver=$1
-    local url="${MIRROR}/drill-${ver}/apache-drill-${ver}.tar.gz.sha512"
+    local f="apache-drill-${ver}.tar.gz"
+    local url="${MIRROR}/${ver}/${f}"
+    local l="${DIR}/${f}"
+    if [ ! -e $l ];
+    then
+        curl -sSLf -o $l $url
+    fi
     printf "  # %s\n" $url
-    printf "  '%s': sha512:%s\n" $ver $(curl -sSL $url)
+    printf "  '%s': sha256:%s\n" $ver $(sha256sum $l | awk '{print $1}')
 }
 
-dl_ver ${1:-1.19.0}
+dl_ver ${1:-1.21.1}
